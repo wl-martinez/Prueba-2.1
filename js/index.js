c37f4d54fffacffae4d873b8eff6b62b9496ac46
wl-martinez <wl.martinez@duocuc.cl>
2023-05-26 22:21:50 -0400

    api rick gato perro

--- js/index.js
+++ js/index.js
@@ -8,18 +8,38 @@ function getCharacters(done) {
 }
 
 getCharacters(data => {
-  data.results.forEach(personaje => {
+  data.results.slice(0, 3).forEach(personaje => { // Utilizamos slice(0, 3) para obtener solo los primeros tres personajes
     const article = document.createRange().createContextualFragment(/*html*/`
-
-    <article>
-    <h5>* ${personaje.name}</h5>
-    <div class="foto-cargo">
-      <img src="${personaje.image}" alt="personaje">
-    </div>
-    </article>
-
-  `);
+      <article>
+        <h5>* ${personaje.name}</h5>
+        <div class="foto-cargo">
+          <img src="${personaje.image}" alt="personaje">
+        </div>
+      </article>
+    `);
     const main = document.querySelector("main");
     main.append(article);
   });
 });
+
+fetch('https://api.thedogapi.com/v1/images/search')
+  .then(response => response.json())
+  .then(data => {
+    const imageUrl = data[0].url;
+    const perroElement = document.querySelector('.perrito');
+    const image = document.createElement('img');
+    image.src = imageUrl;
+    perroElement.appendChild(image);
+  })
+  .catch(error => console.error(error));
+
+  fetch('https://api.thecatapi.com/v1/images/search')
+  .then(response => response.json())
+  .then(data => {
+    const imageUrl = data[0].url;
+    const perroElement = document.querySelector('.gatito');
+    const image = document.createElement('img');
+    image.src = imageUrl;
+    perroElement.appendChild(image);
+  })
+  .catch(error => console.error(error));
